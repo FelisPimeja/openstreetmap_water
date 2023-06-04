cc54568404ef9f4a907a0585fa0251e7f7076ad9
Aleksandr Petrov <felispimeja@gmail.com>
2023-06-05 02:23:53 +0300

    Add html for web page output

--- gen_tiles.bat
+++ gen_tiles.bat
@@ -2,4 +2,4 @@
 FOR /F "eol=# tokens=*" %%i IN (%~dp0\.env) DO SET %%i
 
 @echo on
-ogr2ogr -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 %VECTORTILESDIR% PG:"host=%PGHOST% port=%PGPORT% dbname=%PGDB% user=%PGUSER% password=%PGPASSWORD%" water.waterways_from_rels2
+ogr2ogr -f MVT -dsco FORMAT=DIRECTORY -dsco MAXZOOM=5 -dsco COMPRESS=NO %VECTORTILESDIR% PG:"host=%PGHOST% port=%PGPORT% dbname=%PGDB% user=%PGUSER% password=%PGPASSWORD%" water.waterways_from_rels2
