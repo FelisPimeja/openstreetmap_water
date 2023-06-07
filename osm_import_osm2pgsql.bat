82c5cdbe0a93b958e89f88bd66119074e67fa2de
Aleksandr Petrov <felispimeja@gmail.com>
2023-06-07 13:24:16 +0300

    Add OSM download and proccessing into batch

--- osm_import_osm2pgsql.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-for /F "eol=# tokens=*" %%i in (%~dp0\.env) do set %%i
-
-@echo on
-osm2pgsql -c -d %pgdb% -u %pguser% -h %pghost% -o flex -s %flexConf% --hstore --multi-geometry %osmData%
