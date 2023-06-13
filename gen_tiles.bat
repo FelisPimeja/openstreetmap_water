18099104672804f78c65eecf53b4e5b32d5ce45b
Aleksandr Petrov <felispimeja@gmail.com>
2023-06-14 01:49:43 +0300

    Profiling vector tiles creation

--- gen_tiles.bat
+++ gen_tiles.bat
@@ -5,13 +5,20 @@ for /F "eol=# tokens=*" %%i in (%~dp0\.env) do set %%i
 @echo on
 rmdir %vectorTiles% /s /q
 ogr2ogr -f MVT ^
+    -overwrite ^
+    -progress  ^
+    --debug on ^
     -dsco format=directory ^
+    -dsco tile_extension=pbf ^
+    -dsco conf="vector_tiles_conf.json" ^
     -dsco name="OSM watercourse errors" ^
     -dsco description="Highlighted errors for Openstreetmap watercources validator https://felispimeja.github.io/openstreetmap_water/ (currently Russia only)" ^
     -dsco type=overlay ^
-    -dsco minzoom=3 ^
-    -dsco maxzoom=7 ^
+    -dsco minzoom=0 ^
+    -dsco maxzoom=8 ^
+    -dsco extent=8192 ^
+    -dsco max_size=1000000 ^
     -dsco compress=no ^
     %vectorTiles% ^
     PG:"host=%pghost% port=%pgport% dbname=%pgdb% user=%pguser% password=%pgpassword% active_schema=water" ^
-    water_rels err_watercourse err_spring err_mouth
+    lin_0 lin_1 lin_2 lin_3 lin_4 lin_5 lin_6 lin_7 lin_8
