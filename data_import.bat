4c5f5c80351f6af4336c15abec5c0ea0bbb450b5
Aleksandr Petrov <felispimeja@gmail.com>
2023-06-14 01:48:08 +0300

    Curl retry on errors + create schema water if need

--- data_import.bat
+++ data_import.bat
@@ -8,11 +8,12 @@ for /F "eol=# tokens=*" %%i in (%~dp0\.env) do set %%i
 curl.exe --output %wikiDir%\wikidata.csv ^
     --request POST https://query.wikidata.org/sparql ^
     --header "Accept: text/csv" ^
-    --data-urlencode query@wikidata_watercources.rq
+    --data-urlencode query@wikidata_watercources.rq ^
+    --retry-all-errors
 
 @REM Import Wikidata into DB
 psql -U %pguser% -d %pgdb% -c ^
-    "create table if not exists water.wikidata_waterways_russia (waterway text, labelru text, labelen text, sourcecoords text, mouthcoords text, mouthqid text, length numeric, gvr text, osm_id int8, tributaries text); truncate water.wikidata_waterways_russia;"
+    "create schema if not exists water; create table if not exists water.wikidata_waterways_russia (waterway text, labelru text, labelen text, sourcecoords text, mouthcoords text, mouthqid text, length numeric, gvr text, osm_id int8, tributaries text); truncate water.wikidata_waterways_russia;"
 psql -U %pguser% -d %pgdb% -c "\copy water.wikidata_waterways_russia FROM %wikiDir%\wikidata.csv delimiter ',' csv header"
 
 @REM Download OSM extract
