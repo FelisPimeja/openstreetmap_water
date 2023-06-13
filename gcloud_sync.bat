24dbe101e08ab432b76b057a42d6047c5c5e2786
Aleksandr Petrov <felispimeja@gmail.com>
2023-06-14 01:49:00 +0300

    No need to remove before write + supress errors

--- gcloud_sync.bat
+++ gcloud_sync.bat
@@ -3,14 +3,9 @@
 for /F "eol=# tokens=*" %%i in (%~dp0\.env) do set %%i
 
 @echo on
-@REM Create and copy empty file to the bucket to supress error... 
-@REM ...before purging it if it's empty already
-copy nul empty.txt
-call gcloud storage cp empty.txt gs://%bucketId%
-del empty.txt
-
-@REM Purge bucket subdirectories
-call gcloud storage rm gs://%bucketId%/**
-
 @REM Copy vector tiles dir into the bucket
-call gcloud storage cp --recursive %vectorTiles% gs://%bucketId%
+call gcloud storage cp ^
+    --recursive ^
+    --continue-on-error ^
+    --gzip-in-flight-all ^
+    %vectorTiles% gs://%bucketId%
