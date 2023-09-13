3a5f820a87953a279e9f31fb8a99d0602ede1fda
Aleksandr Petrov <felispimeja@gmail.com>
2023-09-13 22:10:45 +0300

    typo in run.bat

--- run.bat
+++ run.bat
@@ -2,7 +2,6 @@ call download_wikidata.bat
 call import_wikidata.bat
 call download_osm.bat
 call import_osm.bat
-call download_wikidata.bat
 call proccess_data.bat
 call generate_tiles.bat
 call gcloud_sync.bat
